878f930231f4c5e3dd9cdf7fd2cdca03061f128b
Sebastian Czarnota <sebastian.czarnota@gmail.com>
2017-12-15 14:15:04 +0100

    Fix problems with logging when using tqdm.

--- sacred/run.py
+++ sacred/run.py
@@ -254,7 +254,7 @@ class Run(object):
             return
         text = self._output_file.get()
         if isinstance(text, bytes):
-            text = text.decode('utf-8')
+            text = text.decode('utf-8', 'ignore')
         if self.captured_out:
             text = self.captured_out + text
         if self.captured_out_filter is not None:
